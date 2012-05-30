104e7190fb214b1f71c86b894d83cac546f2d128
milktea <milktea.cg7@gmail.com>
2012-05-30 12:45:45 +0900

    もろもろ修正

--- ogpchecker/content/ogpchecker.js
+++ ogpchecker/content/ogpchecker.js
@@ -1,7 +1,8 @@
 var OGPchecker = {
 	check: function() {
-		var url = 'https://developers.facebook.com/tools/debug/og/object?q=' + window._content.document.location.href;
-		window._content.document.location.href = url;
+		var url = "https://developers.facebook.com/tools/debug/og/object?q=";
+		url += encodeURI(window.content.document.location.href);
+		window.content.document.location.href = url;
 	}
 };
 
